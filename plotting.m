d50a3a59a0ff83fb27b25aed686fb0353333b948
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2021-03-11 18:31:16 +0100

    added maxima growth plotting

--- plotting.m
+++ plotting.m
@@ -3,3 +3,19 @@ A = csvread('average_overhead_2021-03-09 23:40:50.896417.csv',...
             1,0);
 
 histogram(A(:,2),100)
+
+
+l = size(A,1);
+maxima = zeros(1,l);
+maxima(1)=A(1,2);
+
+for i = 2:l 
+    if maxima(i-1)>A(i,2)
+        maxima(i)=maxima(i-1);
+    else
+        maxima(i)=A(i,2);
+    end
+end
+
+figure
+stairs(maxima)
